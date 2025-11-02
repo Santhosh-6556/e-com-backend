0f683a866ecc3219aa02f6bf35f99e7c267a1b67
Gokul-pga <gokulanand2508@gmail.com>
2025-11-02 17:15:05 +0530

    app js file changed

--- app.js
+++ app.js
@@ -18,7 +18,11 @@ import order from "./routes/order.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
